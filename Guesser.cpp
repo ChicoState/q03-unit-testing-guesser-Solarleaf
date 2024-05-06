a82107f38915fc5591790c119dbad88433d05304
Solarleaf <124109321+Solarleaf@users.noreply.github.com>
2024-05-06 10:31:25 -0700

    Submit

--- Guesser.cpp
+++ Guesser.cpp
@@ -1,6 +1,8 @@
 #include "Guesser.h"
 #include <string>
 
+#include <iostream>
+
 using std::string;
 
 /*
@@ -14,8 +16,33 @@ using std::string;
   the secret; in other words, if m_secret has a 10 characters and the guess
   has 100, the distance is 10.
 */
-unsigned int Guesser::distance(string guess){
-  return 0;
+
+// PRIVATE
+unsigned int Guesser::distance(string guess)
+{
+  // Checks difference in length
+  int counter{0};
+  size_t l_guess = guess.length();
+  size_t l_secret = m_secret.length();
+  counter = abs(l_guess - l_secret);
+  if (l_guess > l_secret)
+  {
+    guess = guess.substr(0, l_secret);
+  }
+  // Checks difference in character
+  // The check for both makes it so if the guess is shorter not double penalized for diff letters
+  for (size_t k = 0; k < l_guess && k < l_secret; k++)
+  {
+    if (m_secret[k] == guess[k])
+    {
+      continue;
+    }
+    ++counter;
+  }
+  if(counter > l_secret) {
+    counter = l_secret;
+  }
+  return counter;
 }
 
 /*
@@ -24,8 +51,11 @@ unsigned int Guesser::distance(string guess){
   of any Guesser object and must have a length of 32 characters or less,
   otherwise, it will be truncated at that length.
 */
-Guesser::Guesser(string secret){
 
+Guesser::Guesser(string secret){
+  m_remaining = 0;
+  m_secret = secret;
+  // m_secret = "AabB1234!@#keazZ\naa";
 }
 
 /*
@@ -40,7 +70,27 @@ Guesser::Guesser(string secret){
   and the secret.
 */
 bool Guesser::match(string guess){
-  return true;
+  // We don't want a count down because m_remaining is an unsigned, so you could have an overflow
+  // Which would give you a lot of guesses
+
+  if (remaining() >= 3)
+  {
+    return false;
+  }
+  else if (guess == m_secret)
+  {
+    m_remaining = 0;
+    return true;
+  }
+  // This is the null check and the overflow check
+  int temp = remaining();
+  m_remaining += distance(guess);
+  if (temp >= m_remaining || temp < 0)
+  {
+    m_remaining = 5;
+    // std::cerr<<"Something went wrong\n";
+  }
+  return false;
 }
 
 /*
@@ -51,6 +101,6 @@ bool Guesser::match(string guess){
   reset to three (3).
 */
 unsigned int Guesser::remaining(){
-  return 0;
+  return m_remaining;
 }
 
--- GuesserTest.cpp
+++ GuesserTest.cpp
@@ -5,6 +5,8 @@
 #include <gtest/gtest.h>
 #include "Guesser.h"
 
+#include <iostream>
+
 class GuesserTest : public ::testing::Test
 {
 	protected:
@@ -14,9 +16,111 @@ class GuesserTest : public ::testing::Test
 		virtual void TearDown(){} //clean up after each test, (before destructor)
 };
 
-/* Example test
-TEST(GuesserTest, smoke_test)
+
+// This should pass. Password should pass if it's the right password
+TEST(GuesserTest, pass_same)
 {
-    ASSERT_EQ( 1+1, 2 );
+	Guesser guess_test("AabB1234 !@ #keazZnaa");
+	std::string over_32 = "AabB1234 !@ #keazZnaa";
+	bool b_test = guess_test.match(over_32);
+	// std::cout << "Remaining chances: " << guess_test.remaining() << '\n';
+	ASSERT_TRUE(b_test);
 }
-*/
+
+// This should fail - Length test too high
+TEST(GuesserTest, pass_long)
+{
+	Guesser guess_test("AabB1234 !@ #keazZnaa");
+	std::string over_32 = "AabB1234 !@ #keazZnaa6789123456789123456789";
+	bool b_test = guess_test.match(over_32);
+	// std::cout << "Remaining chances: " << guess_test.remaining() << '\n';
+	ASSERT_FALSE(b_test);
+}
+// This should fail - Length test too low
+TEST(GuesserTest, pass_short)
+{
+	Guesser guess_test("AabB1234 !@ #keazZnaa");
+	std::string over_32 = "AabB1";
+	// std::cout << "Remaining chances: " << guess_test.remaining() << '\n';
+	bool b_test = guess_test.match(over_32);
+	ASSERT_FALSE(b_test);
+}
+
+// This should pass - Testing not double counting on too low
+TEST(GuesserTest, pass_short_diff)
+{
+	Guesser guess_test("Aab");
+	std::string over_32 = "bb";
+	// std::cout << "Remaining chances: " << guess_test.remaining() << '\n';
+	guess_test.match(over_32);
+	ASSERT_EQ(3, guess_test.remaining());
+}
+
+// Three Wrong Tests
+TEST(GuesserTest, pass_four_tests)
+{
+	std::string pass = "Aa";
+	Guesser guess_test(pass);
+	std::string over_32 = "Aab";
+	// std::cout << "Remaining chances: " << guess_test.remaining() << '\n';
+	guess_test.match(over_32);
+	// std::cout << "Remaining chances: " << guess_test.remaining() << '\n';
+	guess_test.match(over_32);
+	// std::cout << "Remaining chances: " << guess_test.remaining() << '\n';
+	guess_test.match(over_32);
+	// std::cout << "Remaining chances: " << guess_test.remaining() << '\n';
+	guess_test.match(over_32);
+	// std::cout << "Remaining chances: " << guess_test.remaining() << '\n';
+	// Expects three wrong, as once 3 it stops adding to the wrong
+	ASSERT_EQ(3, guess_test.remaining());
+}
+
+// Two wrong, one right, two wrong
+TEST(GuesserTest, pass_twofail)
+{
+	std::string pass = "Aa";
+	Guesser guess_test(pass);
+	std::string over_32 = "Aab";
+	// Two wrong
+	guess_test.match(over_32);
+	guess_test.match(over_32);
+	// One Right
+	guess_test.match(pass);
+	// Two wrong
+	guess_test.match(over_32);
+	guess_test.match(over_32);
+	// std::cout << "Remaining chances: " << guess_test.remaining() << '\n';
+
+	ASSERT_EQ(2, guess_test.remaining());
+}
+
+// Three Wrong, One Right, Should Fail
+TEST(GuesserTest, pass_lockedout)
+{
+	std::string pass = "Aa";
+	Guesser guess_test(pass);
+	std::string over_32 = "Aab";
+	// Three wrong, should lock
+	guess_test.match(over_32);
+	guess_test.match(over_32);
+	guess_test.match(over_32);
+	// One Right
+	bool check = guess_test.match(pass);
+	ASSERT_FALSE(check);
+}
+// With a zero input, things get weird. Null check will fail, in part because of the overflow check
+// This also helps with 100% test coverage
+TEST(GuesserTest, pass_null)
+{
+	std::string pass = "";
+	Guesser guess_test(pass);
+	std::string over_32 = "5";
+	// Three wrong, should lock but won't
+	guess_test.match(over_32);
+	guess_test.match(over_32);
+	guess_test.match(over_32);
+	// std::cout << "Remaining chances: " << guess_test.remaining() << '\n';
+	// One Right
+	bool check = guess_test.match(pass);
+	ASSERT_FALSE(check);
+}
